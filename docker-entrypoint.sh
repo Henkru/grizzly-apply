e74ba37fe6182939545c350dfae455d6c1344509
Henri Nurmi <henri@nurmi.me>
2022-02-14 19:33:27 +0200

    Add support for Grafana defualt envs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+[ -n "$GF_SERVER_ROOT_URL" ] && GRAFANA_URL=$GF_SERVER_ROOT_URL
+[ -n "$GF_SECURITY_ADMIN_USER" ] && GRAFANA_USER=$GF_SECURITY_ADMIN_USER
+[ -n "$GF_SECURITY_ADMIN_PASSWORD" ] && GRAFANA_TOKEN=$GF_SECURITY_ADMIN_PASSWORD
+
 [ -z "$GRAFANA_URL" ]  && echo "GRAFANA_URL is not specified" && exit 1
 
 TIMEOUT=${TIMEOUT:-60}
@@ -12,12 +17,12 @@ done
 
 [ $CNT -ge $TIMEOUT ] && echo "Timeout reached" && exit 1
 
-for f in $(find "$CONFIG_PATH" -type f -name "datasource-*.yml"); do
+for f in $(find "$CONFIG_PATH" -type f -name "datasource-*"); do
     echo Applying datasource: $f
     grr apply $f
 done
 
-for f in $(find "$CONFIG_PATH" -type f -name "dashboard-*.yml"); do
+for f in $(find "$CONFIG_PATH" -type f -name "dashboard-*"); do
     echo Applying dashboard: $f
     grr apply $f
 done
