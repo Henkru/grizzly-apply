74515d3ebfa9b47443f261fc593efe1a03d647df
Henri Nurmi <henri@nurmi.me>
2022-02-14 19:08:36 +0200

    Wait for Grafana to be up and running

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
+[ -z "$GRAFANA_URL" ]  && echo "GRAFANA_URL is not specified" && exit 1
 
-for f in $(find /conf.d -type f -name "datasource-*.yml"); do
+TIMEOUT=${TIMEOUT:-60}
+CONFIG_PATH=${CONFIG_PATH:-"/conf.d"}
+
+echo "Waiting Grafana to be available..."
+for CNT in $(seq $TIMEOUT); do
+    wget $GRAFANA_URL -q -O - > /dev/null && break
+    sleep 2
+done
+
+[ $CNT -ge $TIMEOUT ] && echo "Timeout reached" && exit 1
+
+for f in $(find "$CONFIG_PATH" -type f -name "datasource-*.yml"); do
     echo Applying datasource: $f
     grr apply $f
 done
 
-for f in $(find /conf.d -type f -name "dashboard-*.yml"); do
+for f in $(find "$CONFIG_PATH" -type f -name "dashboard-*.yml"); do
     echo Applying dashboard: $f
     grr apply $f
 done
